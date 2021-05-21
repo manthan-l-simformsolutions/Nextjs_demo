fb13a94ae8309effb3b07c7b3aeb2f6fc9785e21
manthan-l-simformsolutions <manthan.l@simformsolutions.com>
2021-05-21 11:46:07 +0530

    formate changes

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-import Link from 'next/link'
+import Link from "next/link";
 export default function Home() {
   return (
     <>
       <div className="row">
         <div className="col-1 text-center home">
-          <Link href="/posts"><a className="a1">Post</a></Link>
+          <Link href="/posts">
+            <a className="a1">Post</a>
+          </Link>
         </div>
       </div>
     </>
-  )
+  );
 }
